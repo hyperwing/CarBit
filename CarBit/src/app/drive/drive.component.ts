e3275a2122560f8b5b53a8324259c0e865d3035f
hyperwing <drdavidwing@gmail.com>
2018-10-27 18:34:56 -0400

    Drive page location services
    GPS coord added
    speedometer added

--- CarBit/src/app/drive/drive.component.ts
+++ CarBit/src/app/drive/drive.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { RadSideDrawer } from "nativescript-ui-sidedrawer";
 import * as app from "tns-core-modules/application";
 import { Router } from '@angular/router';
+import { isEnabled, enableLocationRequest, getCurrentLocation, watchLocation, distance, clearWatch } from "nativescript-geolocation";
 
 @Component({
     selector: "Drive",
@@ -10,8 +11,38 @@ import { Router } from '@angular/router';
 })
 export class DriveComponent implements OnInit {
 
+    speed: number;
+    latitude: number;
+    longitude: number;
+
+        
+    getLocationData() : Promise<any> {
+        return new Promise((resolve, reject) => {
+            getCurrentLocation({timeout: 1000}).then(location => {
+                resolve(location);
+            }).catch(error => {
+                reject(error);
+            });
+        });  
+    }
+         
+    public updateLocation() {
+        this.getLocationData().then(result => {
+            this.speed = result.speed;
+            this.longitude= result.longitude;
+            this.latitude = result.latitude;
+            console.log("updateLocationspeed: "+result.speed);
+        }, error => {
+            console.error(error);
+        });
+    }  
+
+    updatePage() : void {
+        console.log("update speed :"+this.speed);
+    }
+
     constructor() {
-        // Use the component constructor to inject providers.
+        var id = setInterval(() => this.updateLocation(), 1000);
     }
 
     ngOnInit(): void {
