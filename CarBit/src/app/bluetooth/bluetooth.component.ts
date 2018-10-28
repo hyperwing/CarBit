bdf1451b2aa94ceea5aee1572e87f650b0371209
hyperwing <drdavidwing@gmail.com>
2018-10-28 04:47:00 -0400

    settings ts update
    added more pref file variables
    bluetooth optioning in progress

--- CarBit/src/app/bluetooth/bluetooth.component.ts
+++ CarBit/src/app/bluetooth/bluetooth.component.ts
@@ -5,6 +5,8 @@ import { ObservableArray } from "tns-core-modules/data/observable-array";
 import * as Bluetooth from "nativescript-bluetooth";
 import { BluetoothObject } from "./bluetoothObject";
 import { EventData, booleanConverter, getViewById } from "tns-core-modules/ui/page/page";
+import * as dialogs from "tns-core-modules/ui/dialogs";
+
 
 @Component({
     selector: "Bluetooth",
@@ -12,6 +14,9 @@ import { EventData, booleanConverter, getViewById } from "tns-core-modules/ui/pa
     templateUrl: "./bluetooth.component.html"
 })
 export class BluetoothComponent implements OnInit {
+
+    bluetoothName: string;
+    bluetoothUUID: string;
 
     connectedText: string;
     availableBluetoothDevices: ObservableArray<BluetoothObject>;
@@ -67,16 +72,36 @@ export class BluetoothComponent implements OnInit {
 
     onItemTap(args){
         var bluetoothObject = this.availableBluetoothDevices.getItem(args.index);
-        alert({
-            title: "UUID",
-            message: bluetoothObject.uuid,
-            okButtonText: "Ok"
+        // alert({
+        //     title: "UUID",
+        //     message: bluetoothObject.uuid,
+        //     okButtonText: "Ok"
+        // });
+
+        dialogs.action("Choose car to associate to", "Cancel button text", ["Option1", "Option2"]).then(result => {
+            console.log("Dialog result: " + result);
+            if(result == "Option1"){
+                alert({
+                    title:"option 1",
+                    message: bluetoothObject.uuid,
+                    okButtonText: "done"
+                })
+            }else if(result == "Option2"){
+                alert({
+                    title:"option 2",
+                    message: bluetoothObject.uuid,
+                    okButtonText: "done"
+                })
+            }
         });
+
     }
 
     onDrawerButtonTap(): void {
         const sideDrawer = <RadSideDrawer>app.getRootView();
         sideDrawer.showDrawer();
     }
+
+    
 
 }
