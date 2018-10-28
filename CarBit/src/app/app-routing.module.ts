c66af40dd9ae28069866634664caf10aaf7282a3
Tyler Cai <tylercai@Tylers-MacBook-Pro.local>
2018-10-28 04:58:07 -0400

    Added vehicle and updated settings.html

--- CarBit/src/app/app-routing.module.ts
+++ CarBit/src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ const routes: Routes = [
     { path: "search", loadChildren: "~/app/search/search.module#SearchModule" },
     { path: "bluetooth", loadChildren: "~/app/bluetooth/bluetooth.module#BluetoothModule" },
     { path: "featured", loadChildren: "~/app/featured/featured.module#FeaturedModule" },
+    { path: "vehicle", loadChildren: "~/app/vehicle/vehicle.module#VehicleModule" },
     { path: "settings", loadChildren: "~/app/settings/settings.module#SettingsModule" }
 ];
 
